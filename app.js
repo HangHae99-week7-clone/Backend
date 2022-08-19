2545fcebfa9366322f52d6607f9e52d769e9cf2d
rmadbstjd <rmadbstjd@naver.com>
2022-08-19 18:16:35 +0900

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RoutesComment = require("./routes/reviews");
 const rotuer = require("./routes");
 const { sequelize } = require("./models");
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
