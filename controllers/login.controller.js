5b99e8a2f7a9195c7e8ec7d289546d1d1451106d
Jiyu Jang <apryll7995@gmail.com>
2022-08-20 13:07:51 +0900

    user 로그인 api 기능 구현

---
+++ controllers/login.controller.js
@@ -0,0 +1,23 @@
+const LoginService = require("../services/login.service");
+
+class LoginController {
+  loginService = new LoginService();
+
+  loginUser = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      const token = await this.loginService.loginUser(email, password);
+
+      if (token.error) {
+        res.status(200).json({ result: false, token });
+      }
+
+      res.status(200).json({ result: true, token });
+    } catch (err) {
+      console.log(err);
+      return next(err);
+    }
+  };
+}
+
+module.exports = LoginController;
