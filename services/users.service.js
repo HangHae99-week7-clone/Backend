72ef1627e6425f4b705d98d7b5e411c251c60f7c
Jiyu Jang <apryll7995@gmail.com>
2022-08-20 15:58:15 +0900

    회원탈퇴, 닉네임 수정 API 구현

--- services/users.service.js
+++ services/users.service.js
@@ -1,5 +1,6 @@
 const UserRepository = require("../repositories/users.repository");
 const bcrypt = require("bcrypt");
+const { route } = require("../routes");
 require("dotenv").config();
 
 class UserService {
@@ -10,24 +11,37 @@ class UserService {
     if (password !== confirm) {
       return { result: false, error: "패스워드가 일치하지 않습니다" };
     }
-    const exUserEmail = await this.userRepository.existUserEmail(email);
+    const exUserEmail = await this.userRepository.findUserByEmail(email);
     if (exUserEmail) {
       return { result: false, error: "이메일이 이미 존재합니다" };
     }
-    const exUserNick = await this.userRepository.existUserNick(nickname);
+    const exUserNick = await this.userRepository.findUserByNick(nickname);
     if (exUserNick) {
       return { result: false, error: "닉네임이 이미 존재합니다" };
     }
 
     const hash = await bcrypt.hash(password, 12);
+    await this.userRepository.createUser(email, nickname, hash);
+    return { result: true, email, nickname };
+  };
 
-    const createUserData = await this.userRepository.createUser(
-      email,
-      nickname,
-      hash
-    );
+  changeNick = async (nickname, nicknamechange) => {
+    const dupNick = await this.userRepository.findUserByNick(nicknamechange);
+    if (dupNick) {
+      return { result: false, error: "이미 존재하는 닉네임입니다" };
+    }
+    await this.userRepository.updateNick(nickname, nicknamechange);
+    return { result: true };
+  };
 
-    return createUserData;
+  deleteUser = async (nickname, password) => {
+    const user = await this.userRepository.findUserByNick(nickname);
+    const compareResult = await bcrypt.compare(password, user.password);
+    if (!compareResult) {
+      return { result: false, error: "비밀번호를 다시 한 번 확인해주세요" };
+    }
+    await this.userRepository.deleteUser(nickname);
+    return { result: true };
   };
 }
 
