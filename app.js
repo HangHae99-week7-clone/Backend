43f2486184a786d95c48a8b738fbbc6af8dcf148
rmadbstjd <rmadbstjd@naver.com>
2022-08-22 17:40:27 +0900

    http적용 테스트중

--- app.js
+++ app.js
@@ -5,17 +5,9 @@ const morgan = require("morgan");
 const rotuer = require("./routes");
 const port = 3000;
 const app = express();
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
+
 require("dotenv").config();
-const options = {
-  ca: fs.readFileSync("/etc/letsencrypt/live/yunseong.shop/fullchain.pem"),
-  key: fs.readFileSync("/etc/letsencrypt/live/yunseong.shop/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/yunseong.shop/cert.pem"),
-};
-http.createServer(app).listen(3000);
-https.createServer(options, app).listen(443);
+
 sequelize
   .sync({ force: false })
   .then(() => {
